a6ed49f1cd0ae47d6764b29e66f6ec1a47c40978
lucastakara <lucastakara2@gmail.com>
2023-10-23 20:25:03 -0300

    Updated setup.py and gitignore

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        return req.read().splitlines()
+
 
 setup(
     name='cex_arbitrage',
     version='1.0',
-    packages=['cex_arb', 'cex_arb.entities'],
+    packages=find_packages(exclude=['tests', 'docs']),
+    # Automatically include all packages, excluding any test or documentation packages
+    install_requires=read_requirements(),
     url='',
     license='MIT',
     author='Lucas Takara',
